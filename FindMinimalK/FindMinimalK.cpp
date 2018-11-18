6cf5a5575a00d0b300cffcee0b866ee9082b2a68
leon <sonic4x>
2018-11-18 10:54:59 +0800

    modify FindMinimalK.cpp

--- FindMinimalK/FindMinimalK.cpp
+++ FindMinimalK/FindMinimalK.cpp
@@ -8,6 +8,61 @@
 using namespace std;
 //K=0, return the min 
 
+int partition(int a[], int lo, int hi)
+{
+	if (lo >= hi)
+		return hi;
+	int i = lo;
+	int j = hi + 1;
+	int t = a[lo];
+	int rt;
+	while (i < j)
+	{
+		do
+			i++;
+		while (i < hi && a[i] < t);
+
+		do {
+			j--;
+		} while (a[j] > t);
+
+		if (i > j)
+			break;
+
+		swap(a[i], a[j]);
+	}
+	swap(a[lo], a[j]);  //note here is not swap(t,a[j])
+	// pivot is j now
+	return j;
+}
+
+//non-recursive version
+int FindMinimalK_nonRecursive(int a[], int len, int k)
+{
+	if (k < 0 || k > len - 1)
+	{
+		throw std::exception("invalid input");
+	}
+	
+	int lo = 0;
+	int hi = len - 1;
+	while (true)
+	{
+		int pivot = partition(a, lo, hi);
+		if (pivot > k)
+		{
+			hi = pivot - 1;
+		}
+		else if (pivot < k)
+		{
+			lo = pivot + 1;
+		}
+		else
+		{
+			return a[pivot];
+		}
+	}
+}
 int FindMinimalK_recursive(int a[], int lo, int hi, int k)
 {
 	if (lo >= hi)
@@ -32,7 +87,7 @@ int FindMinimalK_recursive(int a[], int lo, int hi, int k)
 
 		swap(a[i], a[j]);
 	}
-	swap(t, a[j]);
+	swap(t, a[j]);     //problem, should be swap(a[lo], a[j]
 	// pivot is j now
 	if (j < k)
 		rt = FindMinimalK_recursive(a, j + 1, hi, k);
@@ -42,55 +97,17 @@ int FindMinimalK_recursive(int a[], int lo, int hi, int k)
 		rt = a[j];
 	return rt;
 }
-int FindMinimalK(int a[], int lo, int hi, int k)
-{
-	
-	int l = lo;
-	int h = hi;
 
-	while (true)
-	{
-		int i = l;
-		int j = h+1;
-		int pivot = l;
-		while (i < j)
-		{
-			do {
-				i++;
-			} while (i <= hi && a[i] < a[pivot]);
-
-			do {
-				j--;
-			} while (a[j] > a[pivot]);
-
-			if (i > j)
-				break;
-			
-			swap(a[i], a[j]);
-			
-		}
-		swap(a[pivot], a[j]);
-		pivot = j;
-
-		if (pivot == k)
-			return a[pivot];
-		if (pivot < k)
-			l = pivot + 1;
-		else
-			h = pivot - 1;
-
-	}
-
-}
 int main()
 {
 	int a[] = { 3,9, 4, 6, 7, 8,2, 5,11,7 };
 	int len = sizeof(a) / sizeof(a[0]);
-	int k = 3;
-	int MinimalKValue = FindMinimalK(a, 0, len - 1, k);
+	int k = 0;
+	int MinimalKValue = FindMinimalK_nonRecursive(a, len, k);
 	cout << MinimalKValue << endl;
 
-	int minK = FindMinimalK_recursive(a, 0, len - 1, k);
+	int a2[] = { 3,9, 4, 6, 7, 8,2, 5,11,7 };
+	int minK = FindMinimalK_recursive(a2, 0, len - 1, k);
 	cout << minK << endl;
 	getchar();
     return 0;
